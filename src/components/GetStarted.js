9d01f14a2b9817c84c3825d37d99c73db834b8e9
leeowonikoko <leeowonikoko@yahoo.com>
2022-03-24 15:00:50 +0100

    landingPage done

--- src/components/GetStarted.js
+++ src/components/GetStarted.js
@@ -4,11 +4,11 @@ import React from 'react'
 const GetStarted = () => {
   return (
     <div className='getStarted'>
-      <div className='imgContainer'>
+      <div className='imgContainer img-fluid'>
         <div className='content'>
-          <h1 >Get a Job you actually like</h1>
-          <h5 >Create an account and start applying</h5>
-          <button>Get started</button>
+          <h1 className='getStartedHeader'>Get a Job you actually like</h1>
+          <h5 className='getStartedCreate'>Create an account and start applying</h5>
+          <button className='getStartedButton px-5 py-2'>Get started</button>
         </div>
       </div>
     </div>
